b43dadcb2bd0623a59922ffb04ec0fe533a73c68
ShiqiZhou <shizhou@redhat.com>
2017-08-31 09:26:44 +0800

    git push hello.js

---
+++ hello.js
@@ -0,0 +1,10 @@
+function Hello(){
+    var name;
+    this.setName = funtion(thename){
+        name = thename;
+    }
+    this.sayhello = funtion()[
+        console.log('hello' + name);
+    ]
+}
+module.exports = Hello;
